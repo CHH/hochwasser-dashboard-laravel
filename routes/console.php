5a55d55830553645cd6ca74407253842cbe75690
Christoph Hochstrasser <christoph@hochstrasser.io>
2026-02-24 17:03:47 +0100

    Run job every five minutes

--- routes/console.php
+++ routes/console.php
@@ -10,7 +10,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('app:rivers:import')->everyFifteenMinutes();
+Schedule::command('app:rivers:import')->everyFiveMinutes();
 
 Schedule::call(function () {
     RiverLevel::where('timestamp', '<', Date::today()->subDays(8))->delete();
